f9ee1c9ad6060fe8acee88cf54a3c47751584a02
thenareshofficial <thenareshoffcial@gmail.com>
2023-09-02 06:44:18 +0530

    Note Attack

--- note.py
+++ note.py
@@ -1,23 +1,33 @@
 import os
 import colorama
 from colorama import Fore
-from banner import __banner__
+from banner import __note__
 
 colorama.init(autoreset=True) 
 
-__banner__()
+__note__()
 
 def __note__():
 
-        print(f"{Fore.YELLOW}\t NOTE!!! Enter number Only")
-        try:
-                count = int(input(f"{Fore.BLUE}\n[+]\t{Fore.MAGENTA}Enter Number of Notepad : {Fore.GREEN}"))
-
-                for i in range(count):
-                        os.system("start notepad")
-                        print(f"{Fore.GREEN}{i} Openning..") 
-                print(f"{Fore.GREEN}\nDone...\n")
-        except:
-                print(f"{Fore.RED}\nError : Give Correct Value!!!")
+        while True:
+                try:
+                        print(f"{Fore.RED}\t NOTE!!! {Fore.YELLOW}Enter number Only")
+                        print(f"{Fore.BLUE}[+]\t{Fore.GREEN} 0.Exit")
+                        count = int(input(f"{Fore.BLUE}\n[+]\t{Fore.MAGENTA}Enter Number of Notepad : {Fore.GREEN}"))
+                        for i in range(count):
+                                os.system("start notepad")
+                                print(f"{Fore.GREEN}{i} Opening...") 
+                        print(f"{Fore.GREEN}\nDone...\n")
+                        if count == 0:
+                                print(f"{Fore.GREEN}\nExit...\n")
+                                exit()
+                except ValueError:
+                        print(f"{Fore.RED}\nError : Give Correct Value!!!")
+                except KeyboardInterrupt:
+                        print(f"{Fore.RED}\nExit...")
+                        exit()
+                except EOFError:
+                        print(f"{Fore.RED}\nExit...")
+                        exit()
 
 __note__()
