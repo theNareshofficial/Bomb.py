1f47797aebe911a2a7dc62111402ede48a6cdb25
Naresh R <thenareshoffcial@gmail.com>
2024-02-29 12:37:56 +0530

    Banners Animation

---
+++ src/banner.py
@@ -0,0 +1,190 @@
+import os
+import colorama
+from colorama import Fore
+
+colorama.init(autoreset=True)
+
+# Main Page Banner
+
+def _banner_():
+    
+    os.system('cls' or 'clear')
+    
+    print(f"""{Fore.GREEN}
+\t _______                           __                               
+\t/       \                         /  |                              
+\t$$$$$$$  |  ______   _____  ____  $$ |____        ______   __    __ 
+\t$$ |__$$ | /      \ /     \/    \ $$      \      /      \ /  |  /  |
+\t$$    $$< /$$$$$$  |$$$$$$ $$$$  |$$$$$$$  |    /$$$$$$  |$$ |  $$ |
+\t$$$$$$$  |$$ |  $$ |$$ | $$ | $$ |$$ |  $$ |    $$ |  $$ |$$ |  $$ |
+\t$$ |__$$ |$$ \__$$ |$$ | $$ | $$ |$$ |__$$ | __ $$ |__$$ |$$ \__$$ |
+\t$$    $$/ $$    $$/ $$ | $$ | $$ |$$    $$/ /  |$$    $$/ $$    $$ |
+\t$$$$$$$/   $$$$$$/  $$/  $$/  $$/ $$$$$$$/  $$/ $$$$$$$/   $$$$$$$ |
+\t                                                $$ |      /  \__$$ |
+\t                                                $$ |      $$    $$/ 
+\t                                                $$/        $$$$$$/  
+\t {Fore.BLUE}[+]{Fore.GREEN}website :\t{Fore.RED}http://thenareshofficial.free.nf/  
+\t {Fore.BLUE}[+]{Fore.GREEN}GitHub  :\t{Fore.RED}https://github.com/theNareshofficial
+
+--------------------------------------------------------------------------------------------------------------
+""")
+    
+# __banner__()
+
+# CMD Attack Banner
+
+def _cmd_():
+
+    os.system('cls' or 'clear')
+
+    print(f"""{Fore.GREEN}
+\t  ______         __       __        _______  
+\t/      \       /  \     /  |      /       \ 
+\t/$$$$$$  |      $$  \   /$$ |      $$$$$$$  |
+\t$$ |  $$/       $$$  \ /$$$ |      $$ |  $$ |
+\t$$ |            $$$$  /$$$$ |      $$ |  $$ |
+\t$$ |   __       $$ $$ $$/$$ |      $$ |  $$ |
+\t$$ \__/  |      $$ |$$$/ $$ |      $$ |__$$ |
+\t$$    $$/       $$ | $/  $$ |      $$    $$/ 
+\t$$$$$$$/        $$/      $$/       $$$$$$$/  
+                                             
+
+\t {Fore.BLUE}[+]{Fore.GREEN}Instagram : {Fore.RED}thenareshoffcial  
+\t {Fore.BLUE}[+]{Fore.GREEN}GitHub    : {Fore.RED}thenareshofficial                                               
+
+                                 
+""")
+    
+# __cmd__()
+
+# Note Attack Banner
+
+def _note_():
+
+    os.system('cls' or 'clear')
+
+    print(f""" {Fore.GREEN}
+
+\t __    __              __               
+\t/  \  /  |            /  |              
+\t$$  \ $$ |  ______   _$$ |_     ______  
+\t$$$  \$$ | /      \ / $$   |   /      \ 
+\t$$$$  $$ |/$$$$$$  |$$$$$$/   /$$$$$$  |
+\t$$ $$ $$ |$$ |  $$ |  $$ | __ $$    $$ |
+\t$$ |$$$$ |$$ \__$$ |  $$ |/  |$$$$$$$$/ 
+\t$$ | $$$ |$$    $$/   $$  $$/ $$       |
+\t$$/   $$/  $$$$$$/     $$$$/   $$$$$$$/ 
+\t                                      
+                                        
+\t {Fore.BLUE}[+]{Fore.GREEN}Instagram : {Fore.RED}thenareshoffcial  
+\t {Fore.BLUE}[+]{Fore.GREEN}GitHub    : {Fore.RED}thenareshofficial                                       
+
+""")
+    
+# __note__()
+
+# File Attack Banner
+
+def _file_():
+
+    os.system('cls' or 'clear')
+
+    print(f""" {Fore.GREEN}
+
+\t ________        __        __                 
+\t/        |      /  |      /  |                
+\t$$$$$$$$/       $$/       $$ |        ______  
+\t$$ |__          /  |      $$ |       /      \ 
+\t$$    |         $$ |      $$ |      /$$$$$$  |
+\t$$$$$/          $$ |      $$ |      $$    $$ |
+\t$$ |            $$ |      $$ |      $$$$$$$$/ 
+\t$$ |            $$ |      $$ |      $$       |
+\t$$/             $$/       $$/        $$$$$$$/ 
+
+\t {Fore.BLUE}[+]{Fore.GREEN}Instagram : {Fore.RED}thenareshoffcial  
+\t {Fore.BLUE}[+]{Fore.GREEN}GitHub    : {Fore.RED}thenareshofficial 
+
+""")
+    
+# __file__()
+
+# Folder Attack Banner
+
+def _folder_():
+
+
+    os.system('cls' or 'clear')
+
+    print(f""" {Fore.GREEN}
+
+\t ________            __        __                     
+\t/        |          /  |      /  |                    
+\t$$$$$$$$/   ______  $$ |  ____$$ |  ______    ______  
+\t$$ |__     /      \ $$ | /    $$ | /      \  /      \ 
+\t$$    |   /$$$$$$  |$$ |/$$$$$$$ |/$$$$$$  |/$$$$$$  |
+\t$$$$$/    $$ |  $$ |$$ |$$ |  $$ |$$    $$ |$$ |  $$/ 
+\t$$ |      $$ \__$$ |$$ |$$ \__$$ |$$$$$$$$/ $$ |      
+\t$$ |      $$    $$/ $$ |$$    $$ |$$       |$$ |      
+\t$$/        $$$$$$/  $$/  $$$$$$$/  $$$$$$$/ $$/       
+\t                                                    
+\t                                                    
+                                                      
+\t {Fore.BLUE}[+]{Fore.GREEN}Instagram : {Fore.RED}thenareshoffcial  
+\t {Fore.BLUE}[+]{Fore.GREEN}GitHub    : {Fore.RED}thenareshofficial 
+
+           
+""")
+    
+# Explorer Attack Banner
+
+def _explorer_():
+
+    os.system('cls' or 'clear')
+
+    print(f""" {Fore.GREEN}
+
+\t ________                      __                                         
+\t/        |                    /  |                                        
+\t$$$$$$$$/  __    __   ______  $$ |  ______    ______    ______    ______  
+\t$$ |__    /  \  /  | /      \ $$ | /      \  /      \  /      \  /      \ 
+\t$$    |   $$  \/$$/ /$$$$$$  |$$ |/$$$$$$  |/$$$$$$  |/$$$$$$  |/$$$$$$  |
+\t$$$$$/     $$  $$<  $$ |  $$ |$$ |$$ |  $$ |$$ |  $$/ $$    $$ |$$ |  $$/ 
+\t$$ |_____  /$$$$  \ $$ |__$$ |$$ |$$ \__$$ |$$ |      $$$$$$$$/ $$ |      
+\t$$       |/$$/ $$  |$$    $$/ $$ |$$    $$/ $$ |      $$       |$$ |      
+\t$$$$$$$$/ $$/   $$/ $$$$$$$/  $$/  $$$$$$/  $$/        $$$$$$$/ $$/       
+\t                    $$ |                                                  
+\t                    $$ |                                                  
+\t                    $$/                                                   
+        
+                                                      
+\t {Fore.BLUE}[+]{Fore.GREEN}Instagram : {Fore.RED}thenareshoffcial  
+\t {Fore.BLUE}[+]{Fore.GREEN}GitHub    : {Fore.RED}thenareshofficial        
+
+""")
+
+# __explorer__()    
+
+# Chrome Attack Banner
+
+def _chrome_():
+
+    os.system('cls' or 'clear')
+
+    print(f""" {Fore.GREEN}
+
+\t  ______   __                                                   
+\t /      \ /  |                                                  
+\t/$$$$$$  |$$ |____    ______    ______   _____  ____    ______  
+\t$$ |  $$/ $$      \  /      \  /      \ /     \/    \  /      \ 
+\t$$ |      $$$$$$$  |/$$$$$$  |/$$$$$$  |$$$$$$ $$$$  |/$$$$$$  |
+\t$$ |   __ $$ |  $$ |$$ |  $$/ $$ |  $$ |$$ | $$ | $$ |$$    $$ |
+\t$$ \__/  |$$ |  $$ |$$ |      $$ \__$$ |$$ | $$ | $$ |$$$$$$$$/ 
+\t$$    $$/ $$ |  $$ |$$ |      $$    $$/ $$ | $$ | $$ |$$       |
+\t $$$$$$/  $$/   $$/ $$/        $$$$$$/  $$/  $$/  $$/  $$$$$$$/ 
+                                                                
+\t {Fore.BLUE}[+]{Fore.GREEN}Instagram : {Fore.RED}thenareshoffcial  
+\t {Fore.BLUE}[+]{Fore.GREEN}GitHub    : {Fore.RED}thenareshofficial                                                               
+
+""")
+    
+# __chrome__()
